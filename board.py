66ffe453790defb908d7199fda422a524efa8854
Dmitry <45429986+Endlesssuply@users.noreply.github.com>
2023-03-06 22:12:06 -0500

    Update board.py
    Still lacks proper implementation of neighbours dictionary for the Stone class

--- board.py
+++ board.py
@@ -1,84 +1,237 @@
-"""
-This Python script reads Smart Game Format (SGF) files for Go games and checks if they have a valid result.
-If a file does not have a valid result, it can optionally be deleted. The script can process all SGF files
-in a given folder.
-
-Functions:
-- read_sgf(file_name: str, file_directory: str, do_deletion: bool): Reads a single SGF file and checks
-if it has a valid result. If it does not have a valid result and do_deletion is True, the file will be deleted.
-- read_all_sgf_in_folder(folder_directory: str, do_deletion: bool = False): Reads all SGF files in a folder
-and calls read_sgf for each file.
-
-Parameters:
-- file_name (str): The name of the SGF file to read.
-- file_directory (str): The directory where the SGF file is located.
-- do_deletion (bool): If True and the SGF file does not have a valid result, the file will be deleted.
-
-Usage:
-- Modify the value of games_folder_path_absolute or games_folder_path_relative to the path of the folder
-containing the SGF files.
-- Call read_all_sgf_in_folder to process all SGF files in the folder. Set do_deletion to True if you want
-to delete files without a valid result.
-
-Note:
-- requires the os and board modules to be imported
-"""
-import os
-import board as b
-
-
-def read_sgf(file_name: str, file_directory: str, do_deletion: bool):
+# TODO: rewrite so that the board starts with the complete graph (maybe not, coz running time)
+class Board:
     """
-    Reads a single SGF file and checks if it has a valid result. If it does not have a valid result and
-    do_deletion is True, the file will be deleted.
+    A class that represents the game board.
 
-    Args:
-        file_name (str): The name of the SGF file to read.
-        file_directory (str): The directory where the SGF file is located.
-        do_deletion (bool): If True and the SGF file does not have a valid result, the file will be deleted.
+    Attributes:
+        size (int): The size of the board (i.e. the number of rows and columns).
+        grid (list): A 2D list representing the board, containing Stone objects.
     """
-    with open(file_directory + file_name) as sgf_file:
-        header = sgf_file.readline()
-        if header.find('RE') == -1:
-            print("Game does not have a valid result, unusable.")
-            if do_deletion:
-                print("Proceeding with deletion:")
-                try:
-                    os.remove(file_directory + file_name)
-                except FileNotFoundError:
-                    print("Fail. File was not found.")
+
+    def __init__(self, size=9):
+        """
+        Initializes a Board object.
+
+        Args:
+            size (int): The size of the board. Defaults to 9.
+        """
+        self.size = size
+        self.grid = [[Stone(x, y) for y in range(size)] for x in range(size)]
+
+    def __getitem__(self, position):
+        """
+        Returns the Stone object at the specified position.
+
+        Args:
+            position (tuple): A tuple containing the x and y coordinates of the Stone object.
+
+        Returns:
+            Stone: The Stone object at the specified position.
+        """
+        x, y = position
+        return self.grid[x][y]
+
+    def add_stone(self, x, y, color="Neither"):
+        """
+        Adds a Stone object to the board at the specified position.
+
+        Args:
+            x (int): The x-coordinate of the position.
+            y (int): The y-coordinate of the position.
+            color (str): The color of the stone. Defaults to "Neither".
+        """
+        stone = Stone(x, y, color)
+        # TODO: add neighbours implementation
+        self.grid[x][y] = stone
+
+    def get_stone(self, x, y):
+        return self.grid[x][y]
+
+    # TODO: potentially move to Stone
+    def update_neighbours(self):
+        """
+        Updates the neighboring Stone objects for each Stone object on the board.
+        """
+        for x in range(self.size):
+            for y in range(self.size):
+                stone = self.grid[x][y]
+                neighbours = stone.get_neighbours()
+                for neighbour in neighbours:
+                    if neighbour.color != "Neither":
+                        stone.add_neighbour(neighbour)
+                    else:
+                        stone.remove_neighbour(neighbour)
+
+    # pretty useless methods
+    def remove_stones(self, stones):
+        """
+        Removes the specified Stone objects from the board.
+
+        Args:
+            stones (list): A list of Stone objects to remove.
+        """
+        for stone in stones:
+            self.grid[stone.x][stone.y].die()
+
+    def get_dead_stones(self):
+        """
+        Returns a list of Stone objects that should be removed from the board.
+
+        Returns:
+            list: A list of Stone objects that should be removed.
+        """
+        dead_stones = []
+        for x in range(self.size):
+            for y in range(self.size):
+                stone = self.grid[x][y]
+                if stone.color != "Neither" and stone.count_neighbours() >= stone.max_num_neighbours:
+                    dead_stones.append(stone)
+        return dead_stones
+
+    def __str__(self):
+        """Print a visual representation of the board."""
+        ans = "-" * (self.size * 2 + 1) + '\n'
+        for y in range(self.size):
+            row = "|"
+            for x in range(self.size):
+                stone = self.get_stone(x, y)
+                if stone.color == "Black":
+                    row += "○"
+                elif stone.color == "White":
+                    row += "●"
                 else:
-                    print("Success. File deleted.")
+                    row += " "
+                row += "|"
+            ans += row + '\n'
+            ans += "-" * (self.size * 2 + 1) + '\n'
+        return ans
+
+    def print_max_neighbours(self):
+        """for debugging"""
+        ans = "-" * (self.size * 2 + 1) + '\n'
+        for y in range(self.size):
+            row = "|"
+            for x in range(self.size):
+                stone = self.get_stone(x, y)
+                row += str(stone.max_num_neighbours)
+                row += "|"
+            ans += row + '\n'
+            ans += "-" * (self.size * 2 + 1) + '\n'
+        return ans
+
+
+class Stone:
+    """
+    A class representing a stone on a game board.
+
+    Attributes:
+        color (str): The color of the stone ("Black", "White", or "Neither").
+        x (int): The x position of the stone on the board.
+        y (int): The y position of the stone on the board.
+        neighbours (dict): A dict of neighbouring Stone objects with (x, y) as keys.
+
+    Methods:
+        add_neighbour(neighbor): Adds a neighbour to the stone.
+        remove_neighbour(neighbor): Removes a neighbour from the stone.
+        count_neighbours(): Returns the number of neighbours the stone has.
+        die(): Removes the stone and its connections from the board.
+    """
+
+    color: str
+    x: int
+    y: int
+    neighbours: dict[tuple[int, int], ...]
+
+    def __init__(self, x, y, color="Neither"):
+        """
+        Initializes a new stone with the specified color and position.
+
+        Args:
+            color (str, optional): The color of the stone. Defaults to "Neither".
+            x (int): The x position of the stone on the board.
+            y (int): The y position of the stone on the board.
+        """
+        self.color = color
+        self.neighbours = {}
+        self.x = x
+        self.y = y
+        max_neighbours = 4  # default for stones not on edge or corner
+        if x == 0 or x == 8:  # stone is on left or right edge
+            max_neighbours = 3
+        elif y == 0 or y == 8:  # stone is on top or bottom edge
+            max_neighbours = 3
+        if (x == 0 and y == 0) or (x == 0 and y == 8) or (x == 8 and y == 0) or (x == 8 and y == 8):
+            max_neighbours = 2  # stone is in a corner
+        self.max_num_neighbours = max_neighbours
+        # TODO: maybe make it work for other sizes of boards? also potentially cash the whole thing
+
+    def __str__(self):
+        return f'Stone on coordinates {self.x}, {self.y} is {self.color} and has {self.count_neighbours()} neighbours.'
+
+    def add_neighbour(self, neighbour):
+        """
+        Adds a neighbour to the stone if it is adjacent to the stone.
+
+        Args:
+            neighbour (Stone): The neighbouring stone to add.
+        """
+        # Check if the neighbor is adjacent to the current stone
+        if abs(self.x - neighbour.x) + abs(self.y - neighbour.y) == 1:
+            self.neighbours[neighbour.x, neighbour.y] = neighbour
+            neighbour.neighbours[self.x, self.y] = self
         else:
-            print(f"Game has a valid result of {header[header.index('RE') + 2:header.index('KM')]} aka is usable.")
-            sgf_file.readline()
-            sgf_file.readline()
-            game = sgf_file.readline().split(';')
-            game = game[1:-2]
-            board = b.Board(9)
-            for stone in game:
-                x = ord(stone[2]) - 97
-                y = ord(stone[3]) - 97
-                if stone[0] == "B":
-                    board.add_stone(x, y, "Black")
-                elif stone[0] == "W":
-                    board.add_stone(x, y, "White")
-            print(board)
+            print('You fucked up the neighbours')
+            raise Exception
 
+    def remove_neighbour(self, neighbour):
+        """
+        Removes a neighbour from the stone.
 
-def read_all_sgf_in_folder(folder_directory: str, do_deletion: bool = False):
-    """
-    Reads all SGF files in a folder and calls read_sgf for each file.
+        Args:
+            neighbour (Stone): The neighbouring stone to remove.
+        """
+        self.neighbours[(neighbour.x, neighbour.y)] = None
+        neighbour.neighbours[(self.x, self.y)] = None
 
-    Args:
-        folder_directory (str): The directory containing the SGF files.
-        do_deletion (bool): If True and the SGF file does not have a valid result, the file will be deleted.
-    """
-    for file in os.listdir(folder_directory):
-        read_sgf(file, folder_directory, do_deletion)
+    def count_neighbours(self):
+        """
+        Returns the number of neighbours the stone has.
+
+        Returns:
+            int: The number of neighbours the stone has.
+        """
+        return len(self.neighbours)
+
+    def get_neighbours(self):
+        """
+        Returns a set of neighbouring Stone objects.
+
+        Returns:
+            set: A set of neighbouring Stone objects.
+        """
+        return self.neighbours.values()
+
+    # TODO: potentially move to Board
+    def die(self):
+        """
+        Removes the stone and its connections from the board.
+        """
+        for neighbour in self.neighbours.values():
+            neighbour.remove_neighbour(self)
+        self.color = "Neither"
+        self.neighbours = {}
 
 
 if __name__ == '__main__':
-    games_folder_path_absolute = '/Users/dmitriivlasov/Downloads/go9/'
-    games_folder_path_relative = 'games/'
-    read_all_sgf_in_folder(games_folder_path_relative, False)
+    # Create a new 9x9 board
+    board = Board(9)
+    # print(board[(1, 5)])
+    # print(board)
+    # board.add_stone(1, 5, "Black")
+    # print(board[(1, 5)])
+    # print(board)
+    # board.add_stone(1, 6, "White")
+    # print(board[(1, 5)])
+    # print(board[(1, 6)])
+    # print(board)
+    print(board.print_max_neighbours())
