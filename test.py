603941e9c50452092f5110685ad987985e0b1ca8
HansYoloMF <duaindude@gmail.com>
2023-03-30 22:24:46 -0400

    DNR

--- test.py
+++ test.py
@@ -3,57 +3,6 @@ from sgf_reader import read_sgf
 from go_gui import draw_board
 
 
-board = Board(9)
-
-def calculate_score(board:Board) -> tuple[float, float]:
-    """Calculates the score for both players."""
-    black_score, white_score = 0.0, 0.0
-    black_territory, white_territory = 0, 0
-
-    for x in range(board.size):
-        for y in range(board.size):
-            stone = board.get_stone(x, y)
-            if stone.color == "Black":
-                black_score += 1
-            elif stone.color == "White":
-                white_score += 1
-            else:
-                territory_owner = get_territory_owner(board, x, y)
-                if territory_owner == "Black":
-                    black_territory += 1
-                elif territory_owner == "White":
-                    white_territory += 1
-
-    black_score += black_territory
-    white_score += white_territory
-    return black_score, white_score
-
-def get_territory_owner(board:Board, x, y) -> str:
-    """Determines the owner of the territory at the given coordinates."""
-
-    def dfs(x, y, visited):
-        if (x, y) in visited:
-            return set()
-        visited.add((x, y))
-
-        neighbors = board.get_stone(x, y).get_neighbours()
-        # print(neighbors)
-        result = set()
-        for nx, ny in neighbors:
-            if board.is_valid_coord(nx, ny) and (nx, ny) not in visited:
-                stone = board.get_stone(nx, ny)
-                if stone.color == "Neither":
-                    result |= dfs(nx, ny, visited)
-                else:
-                    result.add(stone.color)
-        return result
-
-    visited = set()
-    stone_colors = dfs(x, y, visited)
-    if len(stone_colors) == 1:
-        return next(iter(stone_colors))
-    else:
-        return "Neither"
 
 
 sgf_reader = read_sgf("/2015-06-30T13_28_05.990Z_j15601s5g8gk.sgf","DataSet/2015-Go9", False)
