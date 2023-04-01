9a97088a14b7cf31355c1ed77914d686bb3d77dc
nothing128 <127168302+nothing128@users.noreply.github.com>
2023-03-31 21:07:30 -0400

    Update GoPlayer.py
    added AI

--- GoPlayer.py
+++ GoPlayer.py
@@ -76,3 +76,42 @@ class RandomGoPlayer(AbstractGoPlayer):
                 [(check_stone.x + 1, check_stone.y), (check_stone.x - 1, check_stone.y),
                  (check_stone.x, check_stone.y + 1), (check_stone.x, check_stone.y - 1)])
         return coord
+    
+class SlightlyBetterBlackPlayer(AbstractGoPlayer):
+    """A Go AI that makes the best move given in its subtree."""
+
+    def __init__(self, gt: GameTree, active_game: Game):
+        """Initialize this GoPlayer"""
+        AbstractGoPlayer.__init__(self, gt, active_game)
+
+    def make_move(self) -> tuple:
+        """This function determines how the next move should be made. It
+        plays the best possible move among its given choices, or randomly by choosing its next move randomly from empty 
+        positions in proximity the last move played if the tree is None or if it is a leaf 
+        """
+        if self.gt is None or self.gt.get_subtrees() == []:
+            self.gt = None
+            move_sequence = self.game.moves
+            i = -1
+            check_stone = self.board.get_stone(move_sequence[i][1], move_sequence[i][2])
+
+            while len(check_stone.get_neighbours()) == check_stone.max_num_neighbours:
+                i -= 1
+            check_stone = self.board.get_stone(move_sequence[i][1], move_sequence[i][2])
+            choices = [(check_stone.x + 1, check_stone.y), (check_stone.x - 1, check_stone.y),
+                       (check_stone.x, check_stone.y + 1), (check_stone.x, check_stone.y - 1)]
+            coord = random.choice(choices)
+            while (coord in check_stone.neighbours) or coord[0] in [-1, 9] or coord[1] in [-1, 9]:
+                choices.remove(coord)
+                coord = random.choice(
+                    [(check_stone.x + 1, check_stone.y), (check_stone.x - 1, check_stone.y),
+                     (check_stone.x, check_stone.y + 1), (check_stone.x, check_stone.y - 1)])
+
+            return coord
+        else:
+            max_win_prob = -9999
+            for subtree in self.gt.get_subtrees():
+                if subtree.win_probability > max_win_prob:
+                    max_win_prob = subtree.win_probability
+                    best_choice = subtree
+            return best_choice.move
